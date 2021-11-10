1190de3812b6ab03bfa8154bf8ae488666d0fbf0
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2021-11-10 01:08:14 -0300

    Refactor: style home page

--- source/cache/ba3ae803b2f55398806f858bc4584fac5b31f0b1.php
+++ source/cache/ba3ae803b2f55398806f858bc4584fac5b31f0b1.php
@@ -5,19 +5,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?php echo e(assets('site/asset/css/style.min.css')); ?>">
+    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
     <title><?php echo e($title); ?></title>
 </head>
 <body>
 
     <header class="main_header">
 
-            
+        <div class="main_header_logo">
+            <i class='bx bxs-notepad' ></i>
+            Notes
+        </div>
+
+        <div class="main_header_actions">
+
+            <a href="javascript:">adicionar</a>
+
+            <i class='bx bx-dots-vertical-rounded'></i>
+
+        </div>
 
     </header>
 
     <div class="notes">
 
-    </div>
+        <a href="javascript:">
+            <div class="notes_item">
+                <div class="notes_item_content">
+                    Logotipo da igreja. Cruz, música, notas musicais,
+                    música, coro, pessoas, vermelho imagem vetorial sem royalties 86738820 ⬇ Baixe vetores da ...
+                </div>
+                <div class="notes_item_title">Lista de compras</div>
+            </div>
+        </a>
 
+        <a href="javascript:">
+            <div class="notes_item">
+                <div class="notes_item_content">
+                    Logotipo da igreja. Cruz, música, notas musicais,
+                    música, coro, pessoas, vermelho imagem vetorial sem royalties 86738820 ⬇ Baixe vetores da ...
+                </div>
+                <div class="notes_item_title">Lista de compras</div>
+            </div>
+        </a>
+
+        <a href="javascript:">
+            <div class="notes_item">
+                <div class="notes_item_content">
+                    Logotipo da igreja. Cruz, música, notas musicais,
+                    música, coro, pessoas, vermelho imagem vetorial sem royalties 86738820 ⬇ Baixe vetores da ...
+                </div>
+                <div class="notes_item_title">Lista de compras</div>
+            </div>
+        </a>
+
+        <a href="javascript:">
+            <div class="notes_item">
+                <div class="notes_item_content">
+                    Logotipo da igreja. Cruz, música, notas musicais,
+                    música, coro, pessoas, vermelho imagem vetorial sem royalties 86738820 ⬇ Baixe vetores da ...
+                </div>
+                <div class="notes_item_title">Lista de compras</div>
+            </div>
+        </a>
+
+        <a href="javascript:">
+            <div class="notes_item">
+                <div class="notes_item_content">
+                    Logotipo da igreja. Cruz, música, notas musicais,
+                    música, coro, pessoas, vermelho imagem vetorial sem royalties 86738820 ⬇ Baixe vetores da ...
+                </div>
+                <div class="notes_item_title">Lista de compras</div>
+            </div>
+        </a>
+
+        <a href="javascript:">
+            <div class="notes_item">
+                <div class="notes_item_content">
+                    Logotipo da igreja. Cruz, música, notas musicais,
+                    música, coro, pessoas, vermelho imagem vetorial sem royalties 86738820 ⬇ Baixe vetores da ...
+                </div>
+                <div class="notes_item_title">Lista de compras</div>
+            </div>
+        </a>
+
+        <a href="javascript:">
+            <div class="notes_item">
+                <div class="notes_item_content">
+                    Logotipo da igreja. Cruz, música, notas musicais,
+                    música, coro, pessoas, vermelho imagem vetorial sem royalties 86738820 ⬇ Baixe vetores da ...
+                </div>
+                <div class="notes_item_title">Lista de compras</div>
+            </div>
+        </a>
+
+    </div>
+    
 </body>
 </html><?php /**PATH /var/www/html/note/source/view/site/home.blade.php ENDPATH**/ ?>
