dc56ed60a1f7c2c37ca0388d9877b9214773ca86
YuriSalesdeOliveira <yuri_oli@hotmail.com>
2021-12-04 17:15:23 -0300

    Feature: add helper for read and writ files

--- source/config/helpers.php
+++ source/config/helpers.php
@@ -24,3 +24,56 @@ function logs(string $channel = null): Logger
     $log = new Log(PATH['config']);
     return $log->channel($channel);
 }
+
+function fileWrit(string $file_path, string $content, bool $exists = true): bool
+{
+    /**
+     * Posso adicionar validar a string file_path para ver se possui
+     * um extensão valida no final da string e também informar o possivel
+     * motivo pelo qual a fopen, fwrit ou fclose possa ter retornado false
+     */
+
+    $validation = $exists ? file_exists($file_path) : true;
+
+    if ($validation) {
+
+        if ($file = fopen($file_path, 'a')) return false;
+
+        if (fwrite($file, $content) === false || !fclose($file)) return false;
+
+        return true;
+    }
+}
+
+function fileRead(string $file_path, ?int $rows = null): bool|string
+{
+    if (file_exists($file_path)) {
+
+        if ($file = fopen($file_path, 'r')) return false;
+
+        $content = '';
+
+        if ($rows === null) {
+
+            $file_size = filesize($file_path);
+
+            $content = fread($file, $file_size);
+
+            fclose($file);
+
+            return $content;
+        }
+
+        while ($rows > 0 && !feof($file )) {
+
+            $content .= fgets($file) . '\n';
+
+            --$rows;
+        }
+
+        return $content;
+
+    }
+
+    return false;
+}
